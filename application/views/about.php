7a38bafcb217df66120a6d227ecaf81f3303b934
maartentamboer <maartentamboer@iMac-van-Maarten.local>
2014-03-23 14:39:23 +0100

    Added about page

---
+++ application/views/about.php
@@ -0,0 +1,16 @@
+<div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>MyParts</h1>
+        <p>Component management system</p>
+        <p>Implementation: <a href="https://github.com/maartentamboer/" target="_blank">Maarten Tamboer</a></p>
+        <p>Name: <a href="http://jeroen3.nl/" target="_blank">Jeroen</a></p>
+      </div>
+    </div> <!-- /container -->
+
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+   <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+   <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash.min.js"></script>
+   <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+   <script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
