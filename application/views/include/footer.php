5a0dd392926c0693cec28e17e70f66559d65cb39
maartentamboer <maartentamboer@iMac-van-Maarten.local>
2014-03-23 14:25:56 +0100

    Added Github link to footer

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,7 +1,10 @@
 <div id="footer">
       <div class="container">
       	<p></p>
-        <p class="text-muted text-center">&copy; 2014 - Maarten Tamboer</p>
+        <p class="text-muted text-center">
+            &copy; 2014 - Maarten Tamboer <br/>
+            <a href="https://github.com/maartentamboer/MyParts">Github</a>
+        </p>
       </div>
     </div>
 
